3453a01ed0e75ef47587e6cd5b9648283b8b43c9
boo <ya.ternovskiy@gmail.com>
2019-01-27 21:06:44 +0300

    Input user name

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,29 @@ import Layout from '../components/layout';
 class IndexPage extends Component {
     
     state = { user: null }
-    
+
+    handleKeyUp = evt => {
+        if (evt.keyCode === 13) {
+          const user =  evt.target.value;
+          this.setState({ user });
+        }
+    }
+
     render() {
+        const { user } = this.state;
+
         return(
         <Layout pageTitle="React simple chat">
         <main>
           <div>
+            <span>
+                {
+                    user
+                        ? (<span>Hello {user}</span> )
+                        : `What is your name?`
+                }
+            </span>
+            { !user && <input type="text" onKeyUp={this.handleKeyUp} /> }
           </div>
         </main>
       </Layout>
